56be356792705d9f1f82fc7da65b372e688bc0a0
Guilhem <aze.adrarit@gmail.com>
2022-12-18 21:30:40 +0100

    [SERVER]: Added sha256 computation method

--- server-source/channels/Message.java
+++ server-source/channels/Message.java
@@ -3,6 +3,8 @@ package channels;
 import java.util.Date;
 import java.util.List;
 
+import cryptography.Hash;
+
 public class Message {
 	public int id;
 	public String content;
@@ -11,8 +13,14 @@ public class Message {
 	public Boolean isEdited;
 	public Boolean isPinned;
 	public Boolean isSigned;
+	public String hash;
 
-	public Message() {
-		
+	public Message(String content) {
+
+		//The database will set the id for us, for now we will set it to 0 and change it later
+		this.id = 0;
+		this.content = content;
+		this.sent = new Date();
+		this.hash = Hash.sha256sum(content);
 	}
 }
--- server-source/cryptography/Hash.java
+++ server-source/cryptography/Hash.java
@@ -1,8 +1,27 @@
 package cryptography;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Hash {
-	public static String sha256sum(byte[] input) {
-		//TODO returns sha256sum of an input
-		return "";
+	public static String sha256sum(String input) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			return Hash.toHexadecimalString(md.digest(input.getBytes()));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return new String();
+	}
+
+	public static String toHexadecimalString(byte[] input) {
+		BigInteger number = new BigInteger(1, input);
+		StringBuilder result = new StringBuilder(number.toString(16));
+
+		while (result.length() < 64)
+			result.insert(0, '0');
+
+		return result.toString();
 	}
 }
